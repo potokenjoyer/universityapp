951349285bac3581c9af1d890b755ae13dfc3959
potokenjoyer <potokenjoyer@gmail.com>
2024-06-21 04:13:35 +0300

    first commit (download files on git)

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="main">
+    <div class="input-form">
+      <div class="logoname">
+        <a href="https://mtuci.ru/"><img src="/logoname.png" alt="Logo"></a>
+      </div>      
+      <div class="logo">
+        <img src="/logo.png" alt="Logo">
+      </div>
+      <form>
+        <input placeholder="Логин"/>
+        <input placeholder="Пароль"/>
+        <button class="btn-authorize">
+          <span>Войти</span>
+        </button>
+        <button class="btn-authorize">
+          <span>Войти</span>
+        </button>
+      </form>
+    </div>     
+  </div>    
+
+</template>
+
+<style scoped>
+
+</style>
